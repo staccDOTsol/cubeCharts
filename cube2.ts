52a9a6145aca85df0df3b26d5e7fc785f7b4f1de
Your Name <you@example.com>
2023-07-24 18:09:59 -0400

    cube2 is trying to consume market data and cannot; likely cuz there is none

--- cube2.ts
+++ cube2.ts
@@ -1,44 +1,52 @@
 import { Root } from 'protobufjs';
 import WebSocket from 'ws';
 
-const marketId = 1; // Replace with your market ID
-const ws = new WebSocket(`wss://api.cube.exchange/md/book/${marketId}`);
-
-
-const root = new Root(); // Create a root object
-root.loadSync('./market_data.proto'); // Load your proto file
-const MdMessage = root.lookupType('md.MdMessage'); // Look up your message type
-
 import * as md from './market_data'; // Replace with your types
-const config:  md.Config = {
-  mbp: true, // Enable MBP feeds
-  mbo: true, // Disable MBO feeds
-  trades: true, // Enable recent trades
-  summary: true, // Enable 24h summary
-  klines: [md.KlineInterval.S1, md.KlineInterval.M1] // Enable price klines for 1 minute and 5 minutes
-};
+const is = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]; // Replace with your market ID
+function doit(i: number) {
+
+  const ws = new WebSocket(`wss://api.cube.exchange/md/book/${i}`);
 
 
-
-ws.on('open', () => {
-  console.log(`WebSocket connection to wss://api.cube.exchange/md/book/${marketId} established`);
-
-  const buffer = MdMessage.encode(MdMessage.create(config)).finish(); // Encode your message to a buffer
-
-  // Send the message to the server
-  ws.send(buffer);
-});
-
-ws.on('message', (data: WebSocket.Data) => {
-  const message = JSON.parse(data.toString());
-  console.log(`Received message: ${JSON.stringify(message)}`);
-});
-
-ws.on('close', () => {
-  console.log(`WebSocket connection to wss://api.cube.exchange/md/book/${marketId} closed`);
-});
-
-ws.on('error', (error: Error) => {
-  console.error(`WebSocket error: ${error.message}`);
-});
-
+  const root = new Root(); // Create a root object
+  root.loadSync('./market_data.proto'); // Load your proto file
+  const MdMessage = root.lookupType('md.MdMessage'); // Look up your message type
+  
+  const config:  md.Config = {
+    mbp: false, // Enable MBP feeds
+    mbo: true, // Disable MBO feeds
+    trades: true, // Enable recent trades
+    summary: true, // Enable 24h summary
+    klines: [md.KlineInterval.S1, md.KlineInterval.M1] // Enable price klines for 1 minute and 5 minutes
+  };
+  
+  
+  
+  ws.on('open', () => {
+    console.log(`WebSocket connection to wss://api.cube.exchange/md/book/${i} established`);
+  
+    const buffer = MdMessage.encode(MdMessage.create(config)).finish(); // Encode your message to a buffer
+  
+    // Send the message to the server
+    ws.send(buffer);
+  });
+  
+  ws.on('message', (data: WebSocket.Data) => {
+  
+    console.log(1)
+    const message = JSON.parse(data.toString());
+    console.log(`Received message: ${JSON.stringify(message)}`);
+  });
+  
+  ws.on('close', () => {
+    console.log(`WebSocket connection to wss://api.cube.exchange/md/book/${i} closed`);
+  });
+  
+  ws.on('error', (error: Error) => {
+    console.error(`WebSocket error: ${error.message}`);
+  });
+  
+}
+for (var i of is){
+  doit(i)
+}
